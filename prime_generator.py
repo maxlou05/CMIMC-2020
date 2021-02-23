56e88274937c3beab841e6ee9ac2a4efa774e981
maxlou05 <62454180+maxlou05@users.noreply.github.com>
2021-02-22 19:58:02 -0500

    Update prime_generator.py

--- prime_generator.py
+++ prime_generator.py
@@ -3,7 +3,10 @@ import math
 import sys
 
 TASK_NUM = 6
+M = 200
+N = 8
 
+# Sieve of Eratosthenes - not mine
 def generate_primes(n):
     is_prime = np.ones(n+1,dtype=bool)
     is_prime[0:2] = False
@@ -12,11 +15,13 @@ def generate_primes(n):
             is_prime[i*2::i]=False
     return np.where(is_prime)[0]
 
-primes = generate_primes(200)
-length = math.floor(len(primes)/8)
+# This simple program just fills the lists with unique primes and a 1
+# This is guaranteed to satisfy the requirements, but it is not the best answer, of course
+primes = generate_primes(M)
+length = math.floor(len(primes)/N)
 
 out = open('answer{}.txt'.format(TASK_NUM), 'w')
-for i in range(8):
+for i in range(N):
     n = primes[length*i:length*(i+1)]
     n = np.append(n, 1)
     for t in range(len(n)):
